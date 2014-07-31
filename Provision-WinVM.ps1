c017b1bac734e2c1a0ca286e560c08c645a7ed36
Simon Bates <simon@bitstructures.com>
2014-07-31 16:36:30 -0400

    Add a Firewall exception for Nodejs

--- Provision-WinVM.ps1
+++ Provision-WinVM.ps1
@@ -1,2 +1,16 @@
-# Disable UAC
-# reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_SZ /d 0 /f
+Param (
+    [Parameter(Mandatory=$True)] [String]$ComputerName,
+    [Parameter(Mandatory=$True)] [PSCredential]$Credential
+)
+
+$s = New-PSSession -ComputerName $ComputerName -Credential $Credential
+
+Invoke-Command -Session $s -ScriptBlock {
+    # Add a Firewall exception for Nodejs
+    netsh advfirewall firewall add rule name="Nodejs" dir=in action=allow program="C:\Program Files (x86)\nodejs\node.exe" enable=yes
+
+    # Disable UAC
+    # reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_SZ /d 0 /f
+}
+
+Remove-PSSession $s
