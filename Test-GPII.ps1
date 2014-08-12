ef72097ad724de55ac335c86f56311b2f6dbeae5
Simon Bates <simon@bitstructures.com>
2014-08-12 10:31:12 -0400

    GPII-896: Fix patching -- doesn't work with ..\ in path

--- Test-GPII.ps1
+++ Test-GPII.ps1
@@ -1,4 +1,4 @@
-Set-PSDebug -Trace 1
+Set-PSDebug -Trace 2
 
 # Clone and build GPII
 
@@ -12,13 +12,15 @@ git clone https://github.com/GPII/windows.git
 cd windows
 npm install --ignore-scripts=true
 grunt build
+cd ..
 
 # Patch jqUnit-node.js
 
-& 'C:\Program Files (x86)\Git\bin\patch.exe' -u ..\node_modules\universal\node_modules\jqUnit\lib\jqUnit-node.js $Home\gpii-automation\jqUnit-node.js.patch
+& 'C:\Program Files (x86)\Git\bin\patch.exe' -u node_modules\universal\node_modules\jqUnit\lib\jqUnit-node.js $Home\gpii-automation\jqUnit-node.js.patch
 
 # Run Tests
 
+cd windows
 node tests\acceptanceTests\AcceptanceTests_builtIn.js
 
 exit $LastExitCode
