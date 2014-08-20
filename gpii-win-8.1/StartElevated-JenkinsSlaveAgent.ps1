cf5d0aa3922514b9ef9f3abe7ba4a97a05a4612d
Simon Bates <simon@bitstructures.com>
2014-08-20 15:09:24 -0400

    GPII-917: Run the Jenkins Slave Agent with elevated privileges

--- gpii-win-8.1/StartElevated-JenkinsSlaveAgent.ps1
+++ gpii-win-8.1/StartElevated-JenkinsSlaveAgent.ps1
@@ -2,4 +2,4 @@ Param (
     [Parameter(Mandatory=$True)] [String]$JenkinsJnlpUrl
 )
 
-Start-Process "C:\Program Files (x86)\Java\jre7\bin\java" -ArgumentList @("-jar" "slave.jar" "-jnlpUrl" $JenkinsJnlpUrl) -Verb RunAs
+Start-Process "C:\Program Files (x86)\Java\jre7\bin\java" -ArgumentList @("-jar", "slave.jar", "-jnlpUrl", $JenkinsJnlpUrl) -Verb RunAs
